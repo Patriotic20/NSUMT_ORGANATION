1848090a4991895c46559dee2a5cadb4ab9567c7
Patriotic20 <pterest160@gmail.com>
2025-09-24 20:51:53 +0500

    Add nginx

--- university_student_test/src/main.py
+++ university_student_test/src/main.py
@@ -22,6 +22,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "https://test.api.nsumt.uz"
     ],
     allow_credentials=True,
     allow_methods=["*"],
