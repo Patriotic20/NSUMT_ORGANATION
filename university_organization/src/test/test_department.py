bd5eaa7fa9c73cc4a3e0763b185fd1130af4aa52
Patriotic20 <pterest160@gmail.com>
2025-09-24 20:10:41 +0500

    sds

--- university_organization/src/test/test_department.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-def test_create_department(client):
-    response = client.post(
-        "/departments/create",
-        json={"name": "KMF"}
-    )
-    assert response.status_code == 200
-    # assert response.json() == {"name": "kmf"}
