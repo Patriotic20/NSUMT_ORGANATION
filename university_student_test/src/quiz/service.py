7767c0e42665008fd78eff7be81a607721c97224
Patriotic20 <pterest160@gmail.com>
2025-10-30 16:23:37 +0500

    Add subject api_fix_9

--- university_student_test/src/quiz/service.py
+++ university_student_test/src/quiz/service.py
@@ -13,28 +13,21 @@ class QuizService:
         self.session = session
         self.basic_service = BasicService(session)
 
-    async def create_quiz(self, quiz_data: QuizBase, teacher_id: int ):
-        subject_stmt = select(Subject).where(Subject.id == quiz_data.subject_id , Subject.teacher_id == teacher_id)
-        subject_result = await self.session.execute(subject_stmt)
-        subject = subject_result.scalars().first()
-        if not subject:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail="Subject not found for this teacher",
-            )
-
+    async def create_quiz(self, quiz_data: QuizBase):
         return await self.basic_service.create(model=Quiz, obj_items=quiz_data)
 
-    async def get_quiz_by_id(self, quiz_id: int, teacher_id: int | None = None):
+    async def get_quiz_by_id(self, quiz_id: int):
         return await self.check_by_teacher_id(
-            teacher_id=teacher_id, quiz_id=quiz_id, raise_not_found=True
+            quiz_id=quiz_id, 
+            raise_not_found=True
         )
 
     async def get_all_quiz(
-        self, teacher_id: int | None = None, limit: int = 20, offset: int = 0
+        self,
+        limit: int = 20, 
+        offset: int = 0,
     ):
         return await self.check_by_teacher_id(
-            teacher_id=teacher_id,
             limit=limit,
             offset=offset,
             is_all=True,
@@ -42,17 +35,23 @@ class QuizService:
         )
 
     async def update_quiz(
-        self, quiz_id: int, teacher_id: int | None, quiz_data: QuizUpdate
+        self, 
+        quiz_id: int, 
+        quiz_data: QuizUpdate
     ):
-        await self.check_by_teacher_id(quiz_id=quiz_id, teacher_id=teacher_id, raise_not_found=True)
+        await self.check_by_teacher_id(
+            quiz_id=quiz_id,  
+            raise_not_found=True
+            )
         filters = [Quiz.id == quiz_id]
         return await self.basic_service.update(model=Quiz, filters=filters, update_data=quiz_data)
         
         
                 
-    async def delete_quiz(self, quiz_id: int, teacher_id: int):
+    async def delete_quiz(self, quiz_id: int):
         await self.check_by_teacher_id(
-            quiz_id=quiz_id, teacher_id=teacher_id, raise_not_found=True
+            quiz_id=quiz_id, 
+            raise_not_found=True
         )
         filters = [Quiz.id == quiz_id]
         return await self.basic_service.delete(model=Quiz, filters=filters)
@@ -62,17 +61,14 @@ class QuizService:
         limit: int | None = None,
         offset: int = 0,
         quiz_id: int | None = None,
-        teacher_id: int | None = None,
         is_all: bool = False,
         raise_not_found: bool = False,
     ):
         filters = []
-        if teacher_id is not None:
-            filters.append(Subject.teacher_id == teacher_id)
         if quiz_id is not None:
             filters.append(Quiz.id == quiz_id)
 
-        stmt = select(Quiz).join(Quiz.subject)
+        stmt = select(Quiz)
 
         if filters:
             stmt = stmt.where(and_(*filters))
