d200492f8abcbf62b7a2e769f93130bcc4cc6b46
Patriotic20 <pterest160@gmail.com>
2025-11-01 16:27:48 +0500

    Add subject api_fix_25

--- university_student_test/src/quiz/api.py
+++ university_student_test/src/quiz/api.py
@@ -26,9 +26,9 @@ def get_quiz_service(session: AsyncSession = Depends(db_helper.session_getter)):
 async def create(
     quiz_data: QuizBase,
     service: QuizService = Depends(get_quiz_service),
-    current_user: TokenPaylod = Depends(require_permission("create:quiz"))
+    _ : TokenPaylod = Depends(require_permission("create:quiz"))
 ):
-    return await service.create_quiz(quiz_data=quiz_data, teacher_id=current_user.user_id,)
+    return await service.create_quiz(quiz_data=quiz_data)
 
 
 @router.post("/upload")
@@ -43,36 +43,36 @@ async def upload_file(file: UploadFile = File(...)):
 async def get_by_id(
     quiz_id: int,
     service: QuizService = Depends(get_quiz_service),
-    current_user: TokenPaylod = Depends(require_permission("read:quiz"))
+    _ : TokenPaylod = Depends(require_permission("read:quiz"))
 ):
-    return await service.get_quiz_by_id(quiz_id=quiz_id, teacher_id=current_user.user_id)
+    return await service.get_quiz_by_id(quiz_id=quiz_id)
 
 @router.get("")
 async def get_all(
     limit: int = 20,
     offset: int = 0,
     service: QuizService = Depends(get_quiz_service),
-    current_user: TokenPaylod = Depends(require_permission("read:quiz"))
+    _ : TokenPaylod = Depends(require_permission("read:quiz"))
 ):
-    return await service.get_all_quiz(limit=limit, offset=offset , teacher_id=current_user.user_id)
+    return await service.get_all_quiz(limit=limit, offset=offset)
 
 @router.patch("/update/{quiz_id}")
 async def update(
     quiz_id: int,
     quiz_data: QuizUpdate,
     service: QuizService = Depends(get_quiz_service),
-    current_user: TokenPaylod = Depends(require_permission("update:quiz"))
+    _ : TokenPaylod = Depends(require_permission("update:quiz"))
 ):
-    return await service.update_quiz(quiz_id=quiz_id, teacher_id=current_user.user_id,  quiz_data=quiz_data)
+    return await service.update_quiz(quiz_id=quiz_id, quiz_data=quiz_data)
 
 
 @router.delete("/delete/{quiz_id}")
 async def delete(
     quiz_id: int,
     service: QuizService = Depends(get_quiz_service),
-    current_user: TokenPaylod = Depends(require_permission("delete:quiz"))
+    _ : TokenPaylod = Depends(require_permission("delete:quiz"))
 ):
-    await service.delete_quiz(quiz_id=quiz_id, teacher_id=current_user.user_id)
+    await service.delete_quiz(quiz_id=quiz_id)
     return {
         "Message" : "Delete successfully"
     }
