b61f583d99f8f2bf1a4387231bd05e65a7c9625f
Patriotic20 <pterest160@gmail.com>
2025-11-04 15:12:25 +0500

    Add subject api_fix_25

--- university_auth/auth/schemas.py
+++ university_auth/auth/schemas.py
@@ -4,4 +4,5 @@ class TokenPaylod(BaseModel):
     valid: bool
     user_id: int | None = None
     username: str | None = None
+    role: str | None = None
     permissions: list[str] = Field(default_factory=list)
