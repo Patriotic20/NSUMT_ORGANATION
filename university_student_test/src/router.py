4e1b8a1286c1b7ae58b83bf41ef7221e33ad91bd
Patriotic20 <pterest160@gmail.com>
2025-11-06 17:12:48 +0500

    Add subject api_fix_25

--- university_student_test/src/router.py
+++ university_student_test/src/router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from question.api import router as question_router
 from quiz.api import router as quiz_router
 from quiz_process.api import router as quiz_process_router
+from result.api import router as result_router
 
 router = APIRouter()
 
@@ -10,3 +11,4 @@ router = APIRouter()
 router.include_router(question_router)
 router.include_router(quiz_router)
 router.include_router(quiz_process_router)
+router.include_router(result_router)
