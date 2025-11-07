8b5b832926a60ab1128c75f3301ef04b87774c92
Patriotic20 <pterest160@gmail.com>
2025-11-07 11:51:30 +0500

    Add subject api_fix_25

--- university_student_test/src/auth/utils/security.py
+++ university_student_test/src/auth/utils/security.py
@@ -14,6 +14,7 @@ from core.config import settings
 from core.database.db_helper import db_helper
 from core.models.user import User
 from core.models.role import Role
+from core.models.student import Student
 
 oauth2_scheme = APIKeyHeader(name="Authorization")
 
@@ -50,13 +51,18 @@ async def validate_token_subscriber(token: str) -> TokenPaylod:
 
     async with db_helper.session_factory() as session:
         user: User | None = await get_user(session, username=username)
+        
+        stmt = select(Student).where(Student.user_id == user.id)
+        result = await session.execute(stmt)
+        student_data = result.scalar_one_or_none()
+        
         if not user:
             return TokenPaylod(valid=False)
 
     return TokenPaylod(
         valid=True,
         user_id=user.id,
-        group_id=payload.get("group_id"),
+        group_id=student_data.group_id,
         username=user.username,
         role=user.roles[0].name if user.roles else None,
         permissions=[p.name for r in user.roles for p in r.permissions],
