e7b5ddfb3780c2c08ba5587a2d327a9ffeb238ee
Patriotic20 <pterest160@gmail.com>
2025-11-11 18:28:35 +0500

    Fix 14

--- university_organization/src/core/models/mixins/timestamp_mixin.py
+++ university_organization/src/core/models/mixins/timestamp_mixin.py
@@ -5,11 +5,12 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 class TimestampMixin:
     created_at: Mapped[datetime] = mapped_column(
+        default=func.now(),
         server_default=func.now(),
         nullable=False,
     )
     updated_at: Mapped[datetime] = mapped_column(
         server_default=func.now(),
         onupdate=func.now(),  
-        nullable=False,
+        nullable=True,
     )
